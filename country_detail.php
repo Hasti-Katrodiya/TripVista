9df713e1f86fa084f66b06d1295ea8fcb4ea1d2d
Hasti-Katrodiya <hastikatrodiya1907@gmail.com>
2023-08-21 16:24:02 +0530

    21/8/23- complete homepage

--- country_detail.php
+++ country_detail.php
@@ -1,7 +1,7 @@
 <?php include('./Includes/header.html');  ?>
 <?php  include('./Configuration/DataConnection.php'); ?>
 
-<div class="body_detail">
+<div class="my-2">
 <?php
 $id = $_GET['id'];
 
@@ -12,13 +12,11 @@ if (mysqli_num_rows($result) > 0) {
   while ($row = mysqli_fetch_array($result)) {
 ?> 
 
-<h1 class="text-center my-3 detail_h1 container-fluid "> <i>Top Tourist Places to Visit in <?php echo $row['Country_Name']; ?></i></h1>
-
-<img src="<?php echo $row['Country_img'];?>" alt="img" height="850px" width="100%" class="container-fluid my-2">
-<hr>
+<h1 class="text-center my-3 detail_h1 container-fluid my-4 py-4">Top Tourist Places to Visit in <span class="color-change"> <?php echo $row['Country_Name']; ?></span></h1>
+<img src="<?php echo $row['Country_img'];?>" alt="img" height="850px" width="100%" class="w-100 my-2">
 <?php  } }?>
 
-<div class="container form_css">
+<div class="container-fluid my-4 py-4 form_css">
     <div class="row">
         <?php 
             $sql = "SELECT * FROM `places` WHERE country_id=$id";
@@ -34,7 +32,7 @@ if (mysqli_num_rows($result) > 0) {
                     <h5 class="card-title text-center"><b><?php echo $row['place_name']; ?></b></h5>
                     <p class="card-text flex-grow-1"><?php echo $row['place_description']; ?></p>
                     <div class="mt-auto">
-                        <button class="btn btn-outline-primary readMoreBtn">Read More ></button>
+                        <button class="btn btn-outline-primary readMoreBtn">Read More >></button>
                     </div>
                 </div>
             </div>
--- index.php
+++ index.php
@@ -5,54 +5,8 @@
 <?php  include('./Configuration/DataConnection.php'); ?>
 
 
-<div class="container mx-auto px-0 mb-4">
-  <div class="row">
-    <div class="col-lg-6 col-md-6 col-sm-12 slide-right ">
-      <h1 class="hero-section">Discover New Horizons.
-      <span class="color-change">Explore the Unexplored.</span>
-      Your Journey Begins Here.</h1>
-      <p class="div-hero-para">"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been..."</p>
-      <button class="btn-explore">Let's Explore >></button>
-    </div>
-    <div class="col-lg-6 col-md-6 col-sm-12">
-    </div>
-  </div>
-</div>
-
-
-
-<h2 class="text-center my-4">We Have <span class="color-change ">Achieve</span></h2>
-<div class="container-fluid   banner_img my-4" >
-    <div class="row my-4 py-5">
-      <div class=" col-md-3 col-sm-6 col-lg-3  ">
-      <i class="fas fa-plane counter-icons"></i>
-      <h3 class="counter_h3">700+</h3>
-      <p class="counter_para">Flight Booking</p>
-      </div>
-      <div class=" col-md-3 col-sm-6 col-lg-3  ">
-      <i class='fas fa-place-of-worship counter-icons'></i>
-      <h3 class="counter_h3">700+</h3>
-      <p class="counter_para">Amazing Tours</p>
-      </div>
-      <div class=" col-md-3 col-sm-6 col-lg-3  ">
-      <i class='fas fa-ship counter-icons'></i>
-      <h3 class="counter_h3">100+</h3>
-      <p class="counter_para">Cruises Booking</p>
-      </div>
-      <div class=" col-md-3 col-sm-6 col-lg-3  ">
-      <i class='	fas fa-ticket-alt counter-icons'></i>
-      <h3 class="counter_h3 ">500+</h3>
-      <p class="counter_para">Ticket Booking</p>
-      </div>
-    
-    </div>
-  </div>
-
-
-
-
 <!-- carousel starts-->
-<!-- <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+<div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
   <div class="carousel-inner carousel_custom">
     <div class="carousel-item active">
       <img src="./Images/slide1.jpg" class="d-block w-100" height="900px" alt="slide1">
@@ -84,44 +38,60 @@
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Next</span>
   </button>
-</div> -->
+</div>
 <!-- carousel End!!-->
 
-<br>
 
-<!-- content with image start-->
-<!-- <h2 class="text-center h2_custom py-2">Explore Unique Cultures and Landscapes</h2>
-<br>
-<div class="container-fluid row my-3">
-    <div class="col-lg-6 col-md-12">
-        <p class="para_custom container-fluid">Our Tours offer an opportunity to immerse yourself in the vibrant cultures and breathtaking landscapes of destinations around the world. From the bustling markets of Marrakech to the serene temples of Kyoto, each tour is crafted to showcase the essence of the region, allowing you to connect with locals, savor authentic cuisine, and witness awe-inspiring natural wonders.</p>
+<div class="container mx-auto  mb-4">
+  <div class="row my-4">
+    <div class="col-lg-6 col-md-6 col-sm-12 slide-right ">
+      <h1 class="content_para">Discover New Horizons.
+      <span class="color-change">Explore the Unexplored.</span>
+      Your Journey Begins Here.</h1>
+      <p class="div_para">"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been..."</p>
+      <button class="btn-explore">Let's Explore >></button>
     </div>
-    <div class="col-lg-6 col-md-12">
-        <img class="img-fluid container-fluid" src="./Images/9.jpg" alt="">
+    <div class="col-lg-6 col-md-6 col-sm-12">
+      <img src="./Images/banner.jpg" alt="img" class="container img_banner">
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis obcaecati molestiae tenetur ea modi quaerat quae repudiandae fuga pariatur delectus!</p>
     </div>
-</div> -->
-<!-- content with image End!!-->
+  </div>
+</div>
 
-<!-- Banner Section Start -->
-<!-- <div class="row container-fluid">
-  <img src="./Images/banner.jpg" alt="banner">
-</div> -->
-<!-- Banner Section End!! -->
-<!-- 
-<br>
-<section>
- <div class="container">
-    <div class="row">
-      <div class="col-md-3">
-        
+
+
+
+
+<h2 class="text-center my-4 py-4 ">We Have <span class="color-change ">Achieve</span></h2>
+<div class="container-fluid  banner_img my-4" >
+    <div class="row my-4 py-5">
+      <div class=" col-md-3 col-sm-6 col-lg-3  ">
+      <i class="fas fa-plane counter-icons"></i>
+      <h3 class="counter_h3">700+</h3>
+      <p class="counter_para">Flight Booking</p>
       </div>
+      <div class=" col-md-3 col-sm-6 col-lg-3  ">
+      <i class='fas fa-place-of-worship counter-icons'></i>
+      <h3 class="counter_h3">700+</h3>
+      <p class="counter_para">Amazing Tours</p>
+      </div>
+      <div class=" col-md-3 col-sm-6 col-lg-3  ">
+      <i class='fas fa-ship counter-icons'></i>
+      <h3 class="counter_h3">100+</h3>
+      <p class="counter_para">Cruises Booking</p>
+      </div>
+      <div class=" col-md-3 col-sm-6 col-lg-3  ">
+      <i class='	fas fa-ticket-alt counter-icons'></i>
+      <h3 class="counter_h3 ">500+</h3>
+      <p class="counter_para">Ticket Booking</p>
+      </div>
+    
     </div>
- </div>
-</section>
-<br> -->
+  </div>
+<br>
 
 <!-- content with image start-->
-<!-- <h2 class="text-center h2_custom py-2">Roaming the Globe: Adventures Await</h2>
+<h2 class="text-center h2_custom py-2">Roaming the Globe: Adventures Await</h2>
 <div class="container-fluid row my-3 ">
     <div class="col-lg-6 col-md-12">
         <img class="img-fluid container-fluid" src="./Images/thailand.jpg" alt="">
@@ -130,15 +100,16 @@
         <p class="para_custom container-fluid">One of the most rewarding aspects of our travel tours is the opportunity to connect with fellow travelers who share your sense of adventure. Forge new friendships, share stories, and create bonds that often last long after the journey ends.</p>
         <p class="para_custom container-fluid">Engage with local communities and learn about their customs and way of life. Our tours prioritize meaningful interactions and create memories that last a lifetime.</p>
     </div>
-</div> -->
+</div>
 <!-- content with image End!!-->
 
 <div class="container">
 <!-- <p class="text-left">TOP DESTINATIONS</p> -->
-<h2 class="text-left h2_custom my-3">EXPLORE POPULAR <span class="color-change"> DESTINATIONS</span> !!</h2> 
+<h2 class="text-left h2_custom my-4 py-4">EXPLORE POPULAR <span class="color-change"> DESTINATIONS</span> !!</h2> 
 </div>
 <!-- Country Display with Animation start -->
- <div class="row container-fluid">
+<div class="container-fluid ">
+ <div class="row">
   <?php
     $sql = "SELECT * FROM `country`";
     $result = mysqli_query($connection,$sql);
@@ -170,7 +141,21 @@
     }
   ?>
 </div> 
+</div>
 
+
+<!-- content with image start-->
+<h2 class="text-center h2_custom py-4 my-4">Explore Unique Cultures and Landscapes</h2>
+<br>
+<div class="container-fluid row my-4 ">
+    <div class="col-lg-6 col-md-12">
+        <p class="para_custom container-fluid">Our Tours offer an opportunity to immerse yourself in the vibrant cultures and breathtaking landscapes of destinations around the world. From the bustling markets of Marrakech to the serene temples of Kyoto, each tour is crafted to showcase the essence of the region, allowing you to connect with locals, savor authentic cuisine, and witness awe-inspiring natural wonders.</p>
+    </div>
+    <div class="col-lg-6 col-md-12">
+        <img class="img-fluid container-fluid" src="./Images/9.jpg" alt="img...">
+    </div>
+</div>
+<!-- content with image End!!-->
 <!-- Country Display with Animation End!! -->
 <script src="./JavaScript/script.js"></script>
 
