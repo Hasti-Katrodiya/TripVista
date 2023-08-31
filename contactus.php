70595b3812742bf1861c4c95086651d5f00294cf
Hasti-Katrodiya <hastikatrodiya1907@gmail.com>
2023-08-31 09:20:33 +0530

    31/8/23

--- contactus.php
+++ contactus.php
@@ -5,12 +5,12 @@
 <?php include('./Configuration/DataConnection.php'); ?>
 
 <div class="background_banner py-4">
-  <h2 class="contactUs_h2">Contact Us</h2>
+  <h2 class="contactUs_h2 display-1">Contact Us</h2>
 </div>
 <section>
   <br><br><br>
 
-  <h1 class="text-center contactUs_h1  py-4">Talk To An <span class="color-change">Expert</span></h1>
+  <h1 class="text-center contactUs_h1 display-2  py-4">Talk To An <span class="color-change">Expert</span></h1>
   <h3 class="contactUs_h3 text-center">Just Share Your Idea. It's Time to Let Your Ideas Grow.</h3>
   <br>
   <br>
@@ -33,7 +33,7 @@
           <label for="exampleFormControlInput1" class="form-label">Subject:</label>
           <input type="email" class="form-control" id="exampleFormControlInput4" placeholder="">
         </div>
-        <div class="mb-3">
+        <div class="mb-3 my-4">
           <label for="exampleFormControlTextarea1" class="form-label">Message:</label>
           <textarea class="form-control" id="exampleFormControlTextarea5" rows="3"></textarea>
         </div>
@@ -43,9 +43,10 @@
       </div>
       <div class="col-md-6 col-lg-6 col-sm-12 my-4 py-4">
         <h3 class="text-center contactUs_h1"><span class="color-change">TripVista</span> Pvt Ltd.</h3>
-        <p class="contactUs_h3 px-4">Lorem ipsum, dolor sit amet consectetur adipisicing elit.corrupti commodi dolor
+        <p class="contactUs_h3  px-4">Lorem ipsum, dolor sit amet consectetur adipisicing elit.corrupti commodi
+          dolor
           cumque ad blanditiis quis sunt architecto.</p>
-        <h5><i class="fas fa-phone-alt px-4 my-4 mx-4 color-change"></i><span class="text-center">(+91) 93270
+        <h5><i class="fas fa-phone-alt px-4 my-4 mx-4 color-change "></i><span class="text-center">(+91) 93270
             88021</span></h5>
         <h5><i class="fas fa-mail-bulk px-4 my-4 mx-4 color-change"></i><span
             class="text-center">tripvista@gmail.com</span></h5>
@@ -56,32 +57,44 @@
 </section>
 <br>
 <br>
-<!-- 
-<h1 class="text-center contactUs_h1  py-4">OUR GLOBAL <span class="color-change">PRESENCE</span></h1>
-<div class="container-fluid my-4 py-4">
+
+<h2 class="text-center contactUs_h1 display-3  py-4">Our Global <span class="color-change">Presence</span></h2>
+
+<div class="container">
   <div class="row">
-    <div class="col-md-4 col-lg-4 col-sm-6 contactUs_Img_Country_div mx-2">
-      hth
-      <img src="./Images/SVGs/1.svg" alt="img..." class="card-img contactUs_Img_Country">
-      <h3 class="my-0 py-0 text-center">India</h3>
-      <p class="my-0 py-0 text-center">Surat, Gujarat</p>
+    <div class="col-sm-6 col-md-2 col-lg-2 py-2 global_country_div">
+      <img src="./Images/SVGs/india.svg" alt="img" height="120px" class="text-center" width="100%">
+      <p class=" text-center py-4">INDIA</p>
     </div>
-    <div class="col-md-4 col-lg-4 col-sm-6 contactUs_Img_Country_div mx-2">
-      htht <img src="./Images/SVGs/2.svg" alt="img..." class="card-img contactUs_Img_Country ">
-      <h3 class="my-0 py-0 text-center">India</h3>
-      <p class="my-0 py-0 text-center">Surat, Gujarat</p>
+    <div class="col-sm-6 col-md-2 col-lg-2 py-2 global_country_div">
+      <img src="./Images/SVGs/australia.svg" alt="img" height="120px" class="text-center" width="100%">
+      <p class=" text-center py-4">Australia</p>
     </div>
-
-    <div class="col-md-4 col-lg-4 col-sm-6 contactUs_Img_Country_div mx-2">
-      htht<img src="./Images/SVGs/2.svg" alt="img..." class="card-img contactUs_Img_Country ">
-      <h3 class="my-0 py-0 text-center">India</h3>
-      <p class="my-0 py-0 text-center">Surat, Gujarat</p>
+    <div class="col-sm-6 col-md-2 col-lg-2 py-2 global_country_div">
+      <img src="./Images/SVGs/dubai.svg" alt="img" height="120px" class="text-center" width="100%">
+      <p class=" text-center py-4">DUBAI</p>
     </div>
-    <div class="col-md-4 col-lg-4 col-sm-6 "></div>
-    <div class="col-md-4 col-lg-4 col-sm-6 "></div>
+    <div class="col-sm-6 col-md-2 col-lg-2 py-2 global_country_div">
+      <img src="./Images/SVGs/newzeland.svg" alt="img" height="120px" class="text-center" width="100%">
+      <p class=" text-center py-4">new-zealand</p>
+    </div>
+    <div class="col-sm-6 col-md-2 col-lg-2 py-2 global_country_div">
+      <img src="./Images/SVGs/uk.svg" alt="img" height="120px" class="text-center" width="100%">
+      <p class=" text-center py-4">UK</p>
+    </div>
+    <div class="col-sm-6 col-md-2 col-lg-2 py-2 global_country_div">
+      <img src="./Images/SVGs/usa.svg" alt="img" height="120px" class="text-center" width="100%">
+      <p class=" text-center py-4">USA</p>
+    </div>
   </div>
-</div> -->
+</div>
 
+
+
+
+
+
+<br><br><br><br><br>
 <!-- Footer File -->
 <?php include('./Includes/footer.html'); ?>
 </body>
--- index.php
+++ index.php
@@ -4,7 +4,6 @@
 <!-- Data Connection -->
 <?php include('./Configuration/DataConnection.php'); ?>
 
-
 <!-- carousel starts-->
 <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
   <div class="carousel-inner carousel_custom">
@@ -45,10 +44,10 @@
 <!-- carousel End!!-->
 
 
-<div class="container mx-auto  mb-4">
+<div class="container-fluid mx-auto  mb-4">
   <div class="row my-4">
-    <div class="col-lg-6 col-md-6 col-sm-12 slide-right ">
-      <h1 class="content_para">Discover New Horizons.
+    <div class="col-lg-6 col-md-6 col-sm-12 slide-right">
+      <h1 class="content_para ">Discover New Horizons.
         <span class="color-change">Explore the Unexplored.</span>
         Your Journey Begins Here.
       </h1>
@@ -58,7 +57,9 @@
     </div>
     <div class="col-lg-6 col-md-6 col-sm-12">
       <img src="./Images/banner.jpg" alt="img" class="container img_banner">
-      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis obcaecati molestiae tenetur ea modi quaerat
+      <p class="para_content">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis obcaecati molestiae
+        tenetur
+        ea modi quaerat
         quae repudiandae fuga pariatur delectus!</p>
     </div>
   </div>
@@ -67,43 +68,67 @@
 
 
 
+<br>
 
-<h2 class="text-center my-4 py-4 ">We Have <span class="color-change ">Achieve</span></h2>
-<div class="container-fluid  banner_img my-4">
-  <div class="row my-4 py-5">
-    <div class=" col-md-3 col-sm-6 col-lg-3  ">
-      <i class="fas fa-plane counter-icons"></i>
-      <h3 class="counter_h3">700+</h3>
-      <p class="counter_para">Flight Booking</p>
-    </div>
-    <div class=" col-md-3 col-sm-6 col-lg-3  ">
-      <i class='fas fa-place-of-worship counter-icons'></i>
-      <h3 class="counter_h3">700+</h3>
-      <p class="counter_para">Amazing Tours</p>
-    </div>
-    <div class=" col-md-3 col-sm-6 col-lg-3  ">
-      <i class='fas fa-ship counter-icons'></i>
-      <h3 class="counter_h3">100+</h3>
-      <p class="counter_para">Cruises Booking</p>
-    </div>
-    <div class=" col-md-3 col-sm-6 col-lg-3  ">
-      <i class='	fas fa-ticket-alt counter-icons'></i>
-      <h3 class="counter_h3 ">500+</h3>
-      <p class="counter_para">Ticket Booking</p>
+<div class="-fluid banner_img my-4 py-4">
+
+  <h2 class="text-center my-4 py-4 text-light display-2">We Have Achieve</h2>
+
+  <div class="row">
+    <div class="col-lg-3 col-md-6 mb-4">
+      <div class="card counter_card">
+        <div class="card-body text-center">
+          <i class="fas fa-plane counter-icons"></i>
+          <h4 class="card-title counter_card_title">Flight Bookings</h4>
+          <p class="card-text counter_para" data-countto="760" data-duration="8000">0+</p>
+        </div>
+      </div>
     </div>
 
+    <div class="col-lg-3 col-md-6 mb-4">
+      <div class="card counter_card">
+        <div class="card-body text-center">
+          <i class='fas fa-place-of-worship counter-icons'></i>
+          <h4 class="card-title counter_card_title">Amazing Tours</h4>
+          <p class="card-text counter_para" data-countto="350" data-duration="8000">0+
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-lg-3 col-md-6 mb-4 ">
+      <div class="card counter_card">
+        <div class="card-body text-center ">
+          <i class='fas fa-ship counter-icons'></i>
+          <h4 class="card-title counter_card_title">Cruises Book</h4>
+          <p class="card-text counter_para" data-countto="750" data-duration="8000">0+</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-lg-3 col-md-6 mb-4">
+      <div class="card counter_card">
+        <div class="card-body text-center">
+          <i class='	fas fa-ticket-alt counter-icons'></i>
+          <h4 class="card-title counter_card_title">Ticket Booking</h4>
+          <p class="card-text counter_para" data-countto="840" data-duration="8000">0+</p>
+        </div>
+      </div>
+    </div>
   </div>
 </div>
 <br>
 
 <!-- content with image start-->
-<h2 class="text-center h2_custom py-2">Roaming the Globe: Adventures Await</h2>
+<h2 class="text-center h2_custom py-4 my-4 display-4">Roaming the Globe:<span class="color-change"> Adventures
+    Await</span></h2>
 <div class="container-fluid row my-3 ">
   <div class="col-lg-6 col-md-12">
-    <img class="img-fluid container-fluid" src="./Images/thailand.jpg" alt="">
+    <img class="img-fluid container-fluid adventure_img" src="./Images/SVGs/plantrip.svg" alt="img">
   </div>
   <div class="col-lg-6 col-md-12">
-    <div class="contetnt_div py-4 px-4 my-4 ">
+    <div class="contetnt_div  py-4 px-4 my-4 ">
+
       <!-- <p class="para_custom container-fluid"> -->
       One of the most rewarding aspects of our travel tours is the opportunity to
       connect with fellow travelers who share your sense of adventure. Forge new friendships, share stories, and create
@@ -120,7 +145,8 @@
 
 <div class="container">
   <!-- <p class="text-left">TOP DESTINATIONS</p> -->
-  <h2 class="text-left h2_custom my-4 py-4">EXPLORE POPULAR <span class="color-change"> DESTINATIONS</span> !!</h2>
+  <h2 class="text-left h2_custom my-4 py-4 display-2">EXPLORE POPULAR <span class="color-change"> DESTINATIONS</span> !!
+  </h2>
 </div>
 <!-- Country Display with Animation start -->
 <div class="container-fluid ">
@@ -160,13 +186,28 @@
   </div>
 </div>
 </div>
+<!-- Country Display with Animation End!! -->
+
 <!-- content with image start-->
-<h2 class="text-center h2_custom py-4 my-4">Explore Unique Cultures and Landscapes</h2>
+<h2 class="text-center h2_custom py-4 my-4 display-3">Explore Unique Cultures and Landscapes</h2>
 <br>
 <div class="container-fluid">
   <div class="row my-4 ">
     <div class="col-lg-6 col-md-12">
-      <p class="para_custom container-fluid">Our Tours offer an opportunity to immerse yourself in the vibrant cultures
+
+      <h3 class="h2_custom text-center my-3 display-5">Exploring New World</h3>
+      <p class="para_custom container-fluid">
+
+
+        Our Tours offer an opportunity to immerse yourself in the vibrant cultures
+        and breathtaking landscapes of destinations around the world. From the bustling markets of Marrakech to the
+        serene temples of Kyoto, each tour is crafted to showcase the essence of the region, allowing you to connect
+        with locals, savor authentic cuisine, and witness awe-inspiring natural wonders.Our Tours offer an opportunity
+        to immerse yourself in the vibrant cultures
+        and breathtaking landscapes of destinations around the world. From the bustling markets of Marrakech to the
+        serene temples of Kyoto, each tour is crafted to showcase the essence of the region, allowing you to connect
+        with locals, savor authentic cuisine, and witness awe-inspiring natural wonders.Our Tours offer an opportunity
+        to immerse yourself in the vibrant cultures
         and breathtaking landscapes of destinations around the world. From the bustling markets of Marrakech to the
         serene temples of Kyoto, each tour is crafted to showcase the essence of the region, allowing you to connect
         with locals, savor authentic cuisine, and witness awe-inspiring natural wonders.</p>
@@ -177,8 +218,8 @@
   </div>
 </div>
 <!-- content with image End!!-->
-<!-- Country Display with Animation End!! -->
-<script src="./JavaScript/script.js"></script>
+
+
 
 <!-- Footer File -->
 <?php include('./Includes/footer.html'); ?>
@@ -186,3 +227,5 @@
 </body>
 
 </html>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js"></script>
+<script src="./JavaScript/script.js"></script>
